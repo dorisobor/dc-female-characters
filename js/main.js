67674783cfeb4a40b22e1354e4508b57048474e2
dorisobor <troyamber@live.com>
2018-04-02 15:26:49 +0200

    updated js file

--- js/main.js
+++ js/main.js
@@ -1,58 +1,72 @@
-//
+const randomizeButton = document
+	.getElementById("randomizeButton");
 
+// a variable for the unique api key
+const my_api = "edd5e498e427ba68da36f8b1693b15fbedde69fa";
 
-// fetchJsonp('http://localhost:3000')
-//   .then(res => res.json())
-//   .then(json => console.log(json));
+let globalSuperheroData = "";
+
 
 fetchSuperheroes();
 
+// function to fetch the json info
+function fetchSuperheroes() {
 
-function fetchSuperheroes(){
-
-  fetch(`https://api-comic-vine.herokuapp.com/characters/?api_key=edd5e498e427ba68da36f8b1693b15fbedde69fa&filter=gender:female&publishers%5B0%5D=4010-10&format=json`)
-    .then((response) => response.json())
-    .then((superheroData) =>  {
-      displaysuperhero(superheroData)
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+	fetch(`https://api-comic-vine.herokuapp.com/characters/?api_key=${my_api}&filter=gender:female&publishers%5B0%5D=4010-10&limit=20&format=json`)
+		.then((response) => response.json())
+		.then((superheroData) => {
+			displayRandomSuperhero(superheroData)
+			//globalSuperheroData = superheroData;
+		})
+		.catch((error) => {
+			console.log(error);
+		})
 }
 
-// a function that will display our character
-function displaysuperhero(superheroData){
-  const results = superheroData.results;
-  const image = superheroData.image;
+// function to fetch the json info
+function displayRandomSuperhero(superheroData) {
+	const results = superheroData.results;
+	const content = document.getElementById("content");
 
-  // get the div id content which all our content will be in
-  const content = document.getElementById('content');
-   const superheroInfo = `
- 
-    <h1> -${results[3].name}-</h1>
-    <div class="boxShadow"></div>
-    <figure><img class ="profileImage" src="${results[3].image.small_url}"> </figure>
-    
+
+	//creates a function to the button so you can randomize 
+	randomizeButton.addEventListener("click", function (superheroData) {
+
+	//for loop to randomize an object 
+
+		var out = "";
+		var i;
+		for (i = 0; i < Math.floor(Math.random() * results.length); i++) {
+
+		}
+
+
+		//  print out the character
+		const superheroInfo = ` 
+    <h1> ${results[i].name}</h1>
+    <div class ="boxShadow"></div>
+    <img class ="profileImage" src="${results[i].image.small_url}"> 
+  
     <div class = "characterInfo">
-    <p> <b>Real name:</b> ${results[3].real_name} </p>
-    <p> <b>Race:</b> ${results[3].origin.name} </p>
-    <p> <b>Appears in:</b> ${results[3].count_of_issue_appearances} issues </p>
-    <p> <b>First Apperance:</b> <i>#${results[3].first_appeared_in_issue.issue_number}</i>
-     ${results[3].first_appeared_in_issue.name}
+
+    <p> <b>Real name:</b> ${results[i].real_name} </p>
+    <p> <b>Race:</b> ${results[i].origin.name} </p>
+    <p> <b>Appears in:</b> ${results[i].count_of_issue_appearances} issues </p>
+    <p> <b>First Apperance:</b> <i>#${results[i].first_appeared_in_issue.issue_number}</i>
+     ${results[i].first_appeared_in_issue.name}
      </p>
-
-    </div>
     <div class="desc">
-    <p> <b>About: </b> ${results[3].deck} </p><div>
+    <p> <b>About: </b> ${results[i].deck} </p><div>
+    </div>
+    `;
+		content.innerHTML = superheroInfo;
 
-   
-  `;
-  // all content will be in here
-  content.innerHTML = superheroInfo;
 
+	});
 
 }
 
-// var dis = "superhero: " + results.aliases + "<br>";
 
-// document.getElementById("content").innerHTML = dis;
+
+
+
